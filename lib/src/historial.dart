1d1b72bf39f35240652de7b6406679214566ade2
GM-DC <gm.flores.pariona@gmail.com>
2021-12-10 08:15:49 -0500

    Proyecto total

--- lib/src/historial.dart
+++ lib/src/historial.dart
@@ -9,9 +9,41 @@ class Historial extends StatelessWidget {
         title: Text('Help Desk del IESTPFFAA'),
       ),
       body: Center(
-        child: Text("My pagina"),
+        child: Column(
+          children: [
+            _targeta1(),
+            SizedBox(height: 15),
+            _targeta1(),
+            SizedBox(height: 15),
+          ],
+        ),
       ),
       drawer: cajaOpciones(context),
     );
   }
+
+
+    Widget _targeta1() {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text("Incidencia COD:00002", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.indigoAccent)),
+            SizedBox(height: 10),
+            Text('Fecha: 01/01/2000    14:50:30'),
+            Text('Tipo de indiencia: Hardware'),
+            Text('Estado: EN PROCESO', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  
+
+  
+
+
 }
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -9,13 +9,92 @@ class home extends StatelessWidget {
         title: Text('Help Desk del IESTPFFAA'),
       ),
       body: Center(
-        child: Text("My pagina"),
+        child: Column(
+          children: [
+            _targeta(),
+            SizedBox(height: 15),
+            _targeta1(),
+            SizedBox(height: 15),
+            _targeta2(),
+            SizedBox(height: 15),
+            _targeta3(),
+          ],
+        ),
       ),
       drawer: cajaOpciones(context),
     );
   }
 
   Widget _targeta() {
-    return Card();
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text("Incidencia COD:00001", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.indigoAccent)),
+            SizedBox(height: 10),
+            Text('Fecha: 01/01/2000    14:50:30'),
+            Text('Tipo de indiencia: Hardware'),
+            Text('Estado: FINALIZADO ', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.blue)),
+          ],
+        ),
+      ),
+    );
   }
+
+  Widget _targeta1() {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text("Incidencia COD:00002", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.indigoAccent)),
+            SizedBox(height: 10),
+            Text('Fecha: 01/01/2000    14:50:30'),
+            Text('Tipo de indiencia: Hardware'),
+            Text('Estado: EN PROCESO', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _targeta2() {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text("Incidencia COD:00003", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.indigoAccent)),
+            SizedBox(height: 10),
+            Text('Fecha: 01/01/2000    14:50:30'),
+            Text('Tipo de indiencia: Hardware'),
+            Text('Estado: EN PROCESO', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.blue)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _targeta3() {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text("Incidencia COD:00003", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.indigoAccent)),
+            SizedBox(height: 10),
+            Text('Fecha: 01/01/2000    14:50:30'),
+            Text('Tipo de indiencia: Hardware'),
+            Text('Estado: PENDIENTE', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 15, color: Colors.orangeAccent)),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
